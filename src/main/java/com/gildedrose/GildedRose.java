6baa3f9e85b0c0dcb3fc03380b7e224c8677b6d0
Tershchenko Matvii <teresenkomatvij@gmail.com>
2024-11-03 20:05:11 +0100

    Refactor GildedRose to use polymorphic update for items

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,48 +9,7 @@ public class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.getName().equals("Aged Brie")
-                    && !item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.getQuality() > 0) {
-                    if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
-                        item.decreaseQuality();
-                    }
-                }
-            } else {
-                if (item.getQuality() < 50) {
-                    item.increaseQuality();
-
-                    if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.getSellIn() < 11) {
-                            item.increaseQuality();
-                        }
-
-                        if (item.getSellIn() < 6) {
-                            item.increaseQuality();
-                        }
-                    }
-                }
-            }
-
-            if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
-                item.decreaseSellIn();
-            }
-
-            if (item.getSellIn() < 0) {
-                if (!item.getName().equals("Aged Brie")) {
-                    if (!item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.getQuality() > 0) {
-                            if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
-                                item.decreaseQuality();
-                            }
-                        }
-                    } else {
-                        item.resetQuality();
-                    }
-                } else {
-                    item.increaseQuality();
-                }
-            }
+            item.update();
         }
     }
 }
